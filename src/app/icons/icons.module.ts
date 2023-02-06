b668a8bf0a36612ff538bfb72c29c66ca5b33f8c
Sthéfano G <sthefano.garcia@paytime.com.br>
2023-02-06 04:10:47 -0300

    feat: added files, more  advancements

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 
 import { FeatherModule } from 'angular-feather';
-
 import { Github, Linkedin, Mail, Globe } from 'angular-feather/icons';
 
 const icons = { Github, Linkedin, Mail, Globe };
