73f14e4e3c2a826c9ce42575d10937a2e397cf74
Sthéfano G <sthefanog@gmail.com>
2023-02-06 15:27:29 -0300

    feat: added work component. added projects (hidden fn). adjusted title counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { IconsModule } from './icons/icons.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { IconsModule } from './icons/icons.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { BioComponent } from './components/bio/bio.component';
 import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { WorkComponent } from './components/work/work.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +21,9 @@ import { AboutComponent } from './components/about/about.component';
     NotFoundComponent,
     BioComponent,
     AboutComponent,
+    ProjectsComponent,
+    WorkComponent,
+    ContactComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, IconsModule],
   providers: [],
