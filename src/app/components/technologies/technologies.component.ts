3fb24d7cf6f5005fa4e3dfdf2af19e79e08341c8
Sthéfano G <sthefanog@gmail.com>
2023-02-10 19:46:11 -0300

    style: adjustments to style. added comment on variables file. moved header and footer css to separate file. made technologies into a loop

--- src/app/components/technologies/technologies.component.ts
+++ src/app/components/technologies/technologies.component.ts
@@ -3,8 +3,47 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-technologies',
   templateUrl: './technologies.component.html',
-  styleUrls: ['./technologies.component.scss']
+  styleUrls: ['./technologies.component.scss'],
 })
 export class TechnologiesComponent {
+  technologies = [
+    {
+      name: 'TS',
+      path: 'typescript',
+    },
+    {
+      name: 'JS',
+      path: 'javascript',
+    },
+    {
+      name: 'HTML',
+      path: 'html5',
+    },
+    {
+      name: 'Angular',
+      path: 'angularjs',
+    },
+    {
+      name: 'Material UI',
+      path: 'materialui',
+    },
+    {
+      name: 'SASS',
+      path: 'sass',
+    },
+    {
+      name: 'Bootstrap',
+      path: 'bootstrap',
+    },
+  ];
 
+  getFullLink(technology: string) {
+    return (
+      'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/' +
+      technology +
+      '/' +
+      technology +
+      '-original.svg'
+    );
+  }
 }
